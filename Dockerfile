d4220271e0397553f5bff285b51b16f902b12bc1
Grant Miiller <grant@m2iller.com>
2019-05-27 12:20:47 -0400

    quality of life improvements

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM node:10-alpine
 
 COPY ./app /usr/src/app
 WORKDIR /usr/src/app
+RUN npm install
 EXPOSE 4000
 CMD [ "npm", "start"]
--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,4 @@ APP_DIR=$PWD/app
 
 MT_VOL="-v $PWD/app/app_files:/usr/src/app/app_files"
 docker pull grantmiiller/gotcha
-docker run $MT_VOL -p 4000:4000 -it --rm --name grantmiiller/gotcha gotcha
+docker run $MT_VOL -p 4000:4000 -it --rm --name gotcha grantmiiller/gotcha
