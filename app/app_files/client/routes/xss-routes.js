673d504f3fb2f7f707d88996af281c55ea049377
Grant Miiller <grant.miiller@nytimes.com>
2019-06-11 13:40:41 -0400

    Renamed XSS

--- app/app_files/client/routes/xss-routes.js
+++ app/app_files/client/routes/xss-routes.js
@@ -4,32 +4,32 @@ const { sanitizeComments } = require('../../helpers/comments');
 function renderComments(req, res) {
     db.getComments(function(comments) {
         const sanitizedComments = sanitizeComments(comments);
-        res.render('xss/dom-xss.html', { title: 'DOM XSS', comments: sanitizedComments });
+        res.render('xss/xss-3.html', { title: 'DOM XSS', comments: sanitizedComments });
     });
 }
 
 function connectXssRoutes(clientApp) {
 
-    clientApp.get('/xss/reflected-server', function(req, res) {
+    clientApp.get('/xss/xss-1', function(req, res) {
         let query = req.query.searchterm || false;
-        res.render('xss/reflected-server-xss.html', { title: 'Server Reflected XSS', query: query });
+        res.render('xss/xss-1.html', { title: 'XSS 1', query: query });
     });
 
-    clientApp.get('/xss/reflected-react', function(req, res) {
-        res.render('xss/reflected-react-xss.html', { title: 'React Reflected XSS' });
+    clientApp.get('/xss/xss-2', function(req, res) {
+        res.render('xss/xss-2.html', { title: 'XSS 2' });
     });
 
-    clientApp.get('/xss/dom', renderComments);
+    clientApp.get('/xss/xss-3', renderComments);
 
-    clientApp.post('/xss/dom', function(req, res) {
+    clientApp.post('/xss/xss-3', function(req, res) {
         let { name, comment } = req.body;
         db.createComment(name, comment, function() {
             renderComments(req, res);
         });
     });
 
-    clientApp.get('/xss/reflected-client', function(req, res) {
-        res.render('xss/reflected-client-xss.html', { title: 'Client Reflected XSS' });
+    clientApp.get('/xss/xss-4', function(req, res) {
+        res.render('xss/xss-4.html', { title: 'Client Reflected XSS' });
     });
 }
 
