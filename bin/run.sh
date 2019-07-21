cfa7db8ca8a836600b4b3f2d398a886aef3616e6
Grant Miiller <grant.miiller@nytimes.com>
2019-07-21 13:28:53 -0400

    Add pack latest pull

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 APP_DIR=$PWD/app
-# docker pull grantmiiller/gotcha
+docker pull grantmiiller/gotcha
 MT_VOL="-v $PWD/app/app_files:/usr/src/app/app_files"
 docker run $MT_VOL -p 4000:4000 -it --rm --name gotcha grantmiiller/gotcha
